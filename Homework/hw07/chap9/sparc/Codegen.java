3eaedee83cd57ebe7e66887fac8dc6489ac5b1d7
Musa V. Ahmed <mahme012@fiu.edu>
2015-05-01 10:58:12 -0400

    ready to test

--- Homework/hw07/chap9/sparc/Codegen.java
+++ Homework/hw07/chap9/sparc/Codegen.java
@@ -125,60 +125,59 @@ public class Codegen {
 
     void munchStm(tree.MOVE s) {
 	  if (s.dst instanceof tree.TEMP) {
-	    tree.TEMP dst = ((tree.TEMP)s.dst);
+	    tree.TEMP dest = ((tree.TEMP)s.dst);
 	    if(s.src instanceof tree.CONST) {
 		  tree.CONST src = ((tree.CONST)s.src);
 		  if(is13bitCONST(s.src)) {
-		    eOPER("\tmov\t" + src.value + ", `d0\n", L(dst.temp, null), null);
+		    eOPER("\tmov\t" + src.value + ", `d0\n", L(dest.temp, null), null);
 		  }
 		  else {
-		    eOPER("\tsethi\t%hi(" + src.value  +"), `d0\n", L(dst.temp, null), null);
-		    eOPER("\tor\t`s0, %lo(" + src.value + "), `d0\n", L(dst.temp, null), L(dst.temp, null));
+		    eOPER("\tsethi\t%hi(" + src.value  +"), `d0\n", L(dest.temp, null), null);
+		    eOPER("\tor\t`s0, %lo(" + src.value + "), `d0\n", L(dest.temp, null), L(dest.temp, null));
 		  }
 	    }
 	    else if(s.src instanceof tree.MEM) {
 		  tree.MEM location = (tree.MEM)s.src;
 		  if(location.exp instanceof tree.BINOP) {
-		    tree.BINOP bexp = (tree.BINOP)location.exp;
-		    if(bexp.binop == tree.BINOP.PLUS) {
-			  if(bexp.left instanceof tree.CONST) {
-			    tree.CONST left = (tree.CONST)bexp.left;
-			    eOPER("\tld\t[`s0 + " + left.value  + "], `d0\n", L(dst.temp, null), L(munchExp(bexp.right, transient3), null));
+		    tree.BINOP binexp = (tree.BINOP)location.exp;
+		    if(binexp.binop == tree.BINOP.PLUS) {
+			  if(binexp.left instanceof tree.CONST) {
+			    tree.CONST left = (tree.CONST)binexp.left;
+			    eOPER("\tld\t[`s0 + " + left.value  + "], `d0\n", L(dest.temp, null), L(munchExp(binexp.right, transient3), null));
 			  }
-			  else if(bexp.right instanceof tree.CONST) {
-			    tree.CONST right = (tree.CONST)bexp.right;
-			    eOPER("\tld\t[`s0 + " + right.value  + "], `d0\n", L(dst.temp, null), L(munchExp(bexp.left, transient3), null));
+			  else if(binexp.right instanceof tree.CONST) {
+			    tree.CONST right = (tree.CONST)binexp.right;
+			    eOPER("\tld\t[`s0 + " + right.value  + "], `d0\n", L(dest.temp, null), L(munchExp(binexp.left, transient3), null));
 			  }
 			  else
-			    eOPER("\tld\t[`s0 + `s1], `d0\n", L(dst.temp, null), L(munchExp(bexp.left, transient3),
-										   L(munchExp(bexp.right, transient2),
+			    eOPER("\tld\t[`s0 + `s1], `d0\n", L(dest.temp, null), L(munchExp(binexp.left, transient3),
+										   L(munchExp(binexp.right, transient2),
 										     null)));
 		    }
 		  }
 		  else
-		    eOPER("\tld\t[`s0 + `s1], `d0\n", L(dst.temp, null),  L(munchExp(s.src), L(frame.g0, null)));
+		    eOPER("\tld\t[`s0 + `s1], `d0\n", L(dest.temp, null),  L(munchExp(s.src), L(frame.g0, null)));
 	    }
 	    else {
-		  eOPER("\tmov\t`s0, `d0\n", L(dst.temp, null), L(munchExp(s.src, null), null));
+		  eOPER("\tmov\t`s0, `d0\n", L(dest.temp, null), L(munchExp(s.src, null), null));
 	    }
 	  }
 	  else if (s.dst instanceof tree.MEM) {
 	    tree.MEM location = (tree.MEM)s.dst;
 	    if(location.exp instanceof tree.BINOP) {
-		  tree.BINOP bexp = (tree.BINOP)location.exp;
-		  if(bexp.binop == tree.BINOP.PLUS) {
-		    if(bexp.left instanceof tree.CONST) {
-			  tree.CONST left = (tree.CONST)bexp.left;
-			  eOPER("\tst\t`s1, [`s0 + " + left.value  + "]\n", null, L(munchExp(bexp.right, transient3),L(munchExp(s.src, transient2), null)));
+		  tree.BINOP binexp = (tree.BINOP)location.exp;
+		  if(binexp.binop == tree.BINOP.PLUS) {
+		    if(binexp.left instanceof tree.CONST) {
+			  tree.CONST left = (tree.CONST)binexp.left;
+			  eOPER("\tst\t`s1, [`s0 + " + left.value  + "]\n", null, L(munchExp(binexp.right, transient3),L(munchExp(s.src, transient2), null)));
 		    }
-		    else if(bexp.right instanceof tree.CONST)
-		    {
-			tree.CONST right = (tree.CONST)bexp.right;
-			eOPER("\tst\t`s1, [`s0 + " + right.value  + "]\n", null, L(munchExp(bexp.left, transient3),L(munchExp(s.src, transient2), null)));
+		    else if(binexp.right instanceof tree.CONST) {
+			  tree.CONST right = (tree.CONST)binexp.right;
+			  eOPER("\tst\t`s1, [`s0 + " + right.value  + "]\n", null, L(munchExp(binexp.left, transient3),L(munchExp(s.src, transient2), null)));
 		    }
 		    else
-			eOPER("\tst\t`s2, [`s0 + `s1]\n", null, L(munchExp(bexp.left),
-								  L(munchExp(bexp.right),
+			  eOPER("\tst\t`s2, [`s0 + `s1]\n", null, L(munchExp(binexp.left),
+								  L(munchExp(binexp.right),
 								    L(munchExp(s.src),null))));
 		  }
 		  else
@@ -301,110 +300,94 @@ public class Codegen {
 	  return reg;
     }
 
-    temp.Temp munchExp(tree.MEM n, temp.Temp r)
-    {
-	if(n.exp instanceof tree.TEMP)
-	{
+    temp.Temp munchExp(tree.MEM n, temp.Temp r) {
+	  if(n.exp instanceof tree.TEMP) {
 	    return ((tree.TEMP)n.exp).temp;
-	}
+	  }
 
-	temp.Temp reg;
-	reg = (r == null) ? new temp.Temp() : r;
-	if(n.exp instanceof tree.BINOP)
-	{
+	  temp.Temp reg;
+	  reg = (r == null) ? new temp.Temp() : r;
+	  if(n.exp instanceof tree.BINOP) {
 	    tree.BINOP bexp = (tree.BINOP)n.exp;
-	    if(bexp.binop == tree.BINOP.PLUS)
-	    {
-		if(bexp.left instanceof tree.CONST)
-		{
+	    if(bexp.binop == tree.BINOP.PLUS) {
+		  if(bexp.left instanceof tree.CONST) {
 		    tree.CONST left = (tree.CONST)bexp.left;
 		    eOPER("\tld\t[`s0 + " + left.value  + "], `d0\n", L(reg, null), L(munchExp(bexp.right, reg), null));
-		}
-		else if(bexp.right instanceof tree.CONST)
-		{
+		  }
+		  else if(bexp.right instanceof tree.CONST) {
 		    tree.CONST right = (tree.CONST)bexp.right;
 		    eOPER("\tld\t[`s0 + " + right.value  + "], `d0\n", L(reg, null), L(munchExp(bexp.left, reg), null));
-		}
-		else
+		  }
+		  else
 		    eOPER("\tld\t[`s0 + `s1], `d0\n", L(reg, null), L(munchExp(bexp.left),
 								      L(munchExp(bexp.right),
 									null)));
 	    }
-	}
-	else
+	  }
+	  else
 	    throw new Error("Error");
-	return reg;
+
+	  return reg;
     }
 
-    temp.Temp munchExp(tree.CALL n, temp.Temp r)
-    {
-	temp.Temp reg;
-	reg = (r == null) ? frame.RVCaller() : r;
+    temp.Temp munchExp(tree.CALL n, temp.Temp r) {
+	  temp.Temp reg;
+	  reg = (r == null) ? frame.RVCaller() : r;
 
-	int argCount = 0;
-	int spCount = 92;
-	tree.ExpList args = n.args;
-	while(args != null)
-	{
-	    if( argCount <= 5)
-	    {
-		if(args.head instanceof tree.TEMP)
-		{
+	  int argCount = 0;
+	  int spCount = 92;
+	  tree.ExpList args = n.args;
+	  while(args != null) {
+	    if( argCount <= 5) {
+		  if(args.head instanceof tree.TEMP) {
 		    tree.TEMP currArg = (tree.TEMP)args.head;
 		    eOPER("\tmov\t`s0, `d0\n", L(frame.outgoingArgs[argCount++], null), L(currArg.temp, null));
-		}
-		else if(args.head instanceof tree.MEM)
-		{
+		  }
+		  else if(args.head instanceof tree.MEM) {
 		    tree.MEM location = (tree.MEM)args.head;
-		    if(location.exp instanceof tree.BINOP)
-		    {
-			tree.BINOP bexp = (tree.BINOP)location.exp;
-			if(bexp.binop == tree.BINOP.PLUS)
-			{
-			    if(bexp.left instanceof tree.CONST)
-			    {
-				tree.CONST left = (tree.CONST)bexp.left;
-				eOPER("\tld\t[`s0 + " + left.value  + "], `d0\n", L(frame.outgoingArgs[argCount++], null), L(munchExp(bexp.right, transient3), null));
+		    if(location.exp instanceof tree.BINOP) {
+			  tree.BINOP bexp = (tree.BINOP)location.exp;
+			  if(bexp.binop == tree.BINOP.PLUS) {
+			    if(bexp.left instanceof tree.CONST) {
+				  tree.CONST left = (tree.CONST)bexp.left;
+				  eOPER("\tld\t[`s0 + " + left.value  + "], `d0\n", L(frame.outgoingArgs[argCount++], null), L(munchExp(bexp.right, transient3), null));
 			    }
-			    else if(bexp.right instanceof tree.CONST)
-			    {
-				tree.CONST right = (tree.CONST)bexp.right;
-				eOPER("\tld\t[`s0 + " + right.value  + "], `d0\n", L(frame.outgoingArgs[argCount++], null), L(munchExp(bexp.left, transient3), null));
+			    else if(bexp.right instanceof tree.CONST) {
+				  tree.CONST right = (tree.CONST)bexp.right;
+				  eOPER("\tld\t[`s0 + " + right.value  + "], `d0\n", L(frame.outgoingArgs[argCount++], null), L(munchExp(bexp.left, transient3), null));
 			    }
 			    else
-				eOPER("\tld\t[`s0 + `s1], `d0\n", L(frame.outgoingArgs[argCount++], null), L(munchExp(bexp.left, transient3),
+				  eOPER("\tld\t[`s0 + `s1], `d0\n", L(frame.outgoingArgs[argCount++], null), L(munchExp(bexp.left, transient3),
 													     L(munchExp(bexp.right, transient2),
 													       null)));
-			}
+			  }
 		    }
 		    else
-			eOPER("\tld\t[`s0 + `s1], `d0\n", L(frame.outgoingArgs[argCount++], null),  L(munchExp(args.head, transient3), L(frame.g0, null)));
-		}
-		else
+			  eOPER("\tld\t[`s0 + `s1], `d0\n", L(frame.outgoingArgs[argCount++], null),  L(munchExp(args.head, transient3), L(frame.g0, null)));
+		  }
+		  else
 		    munchExp(args.head, frame.outgoingArgs[argCount++]);
 	    }
 	    args = args.tail;
-	}
-	tree.NAME nm = (tree.NAME)n.func;
-	assem.InstrList tracer = null;
-	assem.InstrList currInstr = ilist;
-	while(currInstr.tail != null)
-	{
+	  }
+	  tree.NAME nm = (tree.NAME)n.func;
+	  assem.InstrList tracer = null;
+	  assem.InstrList currInstr = ilist;
+	  while(currInstr.tail != null) {
 	    tracer = currInstr;
 	    currInstr = currInstr.tail;
-	}
-	if(tracer != null)
+	  }
+	  if(tracer != null)
 	    tracer.tail = new assem.InstrList(new assem.OPER("\tcall `j0\n", null, null, new temp.LabelList(nm.label, null)), currInstr);
-	else
-	{
+	  else {
 	    eOPER("\tcall `j0\n", null, null, new temp.LabelList(nm.label, null));
 	    eOPER("\tnop\n", null, null);
-	}
-	return reg;
+	  }
+
+	  return reg;
     }
 
     void comment(String com) {
 	  eOPER(com + "\n", null, null);
     }
 }
-
